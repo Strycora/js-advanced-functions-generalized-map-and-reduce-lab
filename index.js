850cfc0910ed80ccebb54a37e2376726a6f32452
Vikram Chatterjee <strycora@gmail.com>
2021-02-25 13:48:44 -0700

    commit tree 07e552d7d86ab23a934c9e12c24977c9aee8f377

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(src, fn) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(fn(src[i])) // Unique work
+  }
+  return r
+}
+
+function reduce(arr, cb, start){
+  let r = (!!start) ? start : arr[0]
+  let i = (!!start) ? 0 : 1
+  for (; i < arr.length ; i++){
+    r = cb(arr[i], r)
+  }
+  return r;
+}
